73c2b32809a0aea286f6e59ce235334bda8051ab
prem-modi <prem.technotery@gmail.com>
2024-04-26 17:57:18 +0530

    add first commit

---
+++ application/controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// print_r(define('BASEPATH')OR exit('No direct script access allowed'));
+// echo "kkk";
+
+class PageController extends CI_Controller 
+{
+    public function index()
+    {
+        echo "ss";
+        // exit;
+    }
+}
+
+
+?>
